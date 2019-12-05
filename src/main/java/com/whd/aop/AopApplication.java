72579476a1aa6a83c8fe395e0950b4c25eadab33
houding.wang <houding.wang@net263.com>
2019-12-05 10:27:17 +0800

    s

--- src/main/java/com/whd/aop/AopApplication.java
+++ src/main/java/com/whd/aop/AopApplication.java
@@ -1,11 +1,20 @@
 package com.whd.aop;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AopApplication {
-
+	
+	@RequestMapping("/")
+	public String hello(){
+		return "hello springboot!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AopApplication.class, args);
 	}
